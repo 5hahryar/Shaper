d0c592579e099310362d8f0aa0730bdbe91c613e
Shahryar Khosravi <36692416+5hahryar@users.noreply.github.com>
2020-08-26 18:31:14 +0430

    minor changes

--- app/src/main/java/com/sloupycom/shaper/view/LoginActivity.kt
+++ app/src/main/java/com/sloupycom/shaper/view/LoginActivity.kt
@@ -18,33 +18,46 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : AppCompatActivity(), LoginActivityViewModel.OnAuthCompleteListener {
 
     /** Values **/
-    private var mBinding: ActivityLoginBinding? = null
+    private lateinit var mBinding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /** Init data binding **/
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        mBinding?.loadingLayout?.visibility = View.VISIBLE
-        mBinding?.viewModel = LoginActivityViewModel(application, this)
+        mBinding.loadingLayout.visibility = View.VISIBLE
+        mBinding.viewModel = LoginActivityViewModel(application, this)
     }
 
+    /**
+     * set onClick for views
+     */
     fun onClick(view: View) {
         when(view.id) {
             R.id.button_login -> signupNewAccount()
         }
     }
 
+    /**
+     * Open google sign-in intent
+     */
     private fun signupNewAccount() {
-        val signInIntent: Intent = mBinding?.viewModel?.mGSC?.signInIntent!!
+        val signInIntent: Intent = mBinding.viewModel?.mGSC?.signInIntent!!
         startActivityForResult(signInIntent, Constants.RC_SIGN_IN)
     }
 
+    /**
+     * Get sign-in results from intent
+     */
     @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == Constants.RC_SIGN_IN){
-            mBinding?.viewModel?.onSignUpIntentResult(data)
+            mBinding.viewModel?.onSignUpIntentResult(data)
         }
     }
 
+    /**
+     * Triggered when authentication is successful
+     */
     override fun onAuthSuccessful(account: FirebaseUser) {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("intent", account)
@@ -52,6 +65,9 @@ class LoginActivity : AppCompatActivity(), LoginActivityViewModel.OnAuthComplete
         finish()
     }
 
+    /**
+     * Triggered when authentication is failed
+     */
     override fun onAuthFailed(error: String) {
         loading_layout.visibility = View.GONE
         Log.d("TAG", error)
