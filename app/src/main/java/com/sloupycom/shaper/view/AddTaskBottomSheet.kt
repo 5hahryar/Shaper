24a0feee87068f68f12e8347c0bbddaf9b11f21e
Shahryar Khosravi <36692416+5hahryar@users.noreply.github.com>
2021-02-09 13:26:54 +0330

    Repetition UI implementation

--- app/src/main/java/com/sloupycom/shaper/view/AddTaskBottomSheet.kt
+++ app/src/main/java/com/sloupycom/shaper/view/AddTaskBottomSheet.kt
@@ -1,11 +1,16 @@
 package com.sloupycom.shaper.view
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.Observable
@@ -16,13 +21,18 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import com.google.android.material.snackbar.Snackbar
 import com.sloupycom.shaper.R
 import com.sloupycom.shaper.databinding.BottomsheetAddTaskBinding
+import com.sloupycom.shaper.utils.TextInputFilter
 import com.sloupycom.shaper.viewmodel.AddTaskViewModel
 import kotlinx.android.synthetic.main.bottomsheet_add_task.*
+import net.cachapa.expandablelayout.ExpandableLayout
+import nl.bryanderidder.themedtogglebuttongroup.ThemedToggleButtonGroup
+
 
 class AddTaskBottomSheet : BottomSheetDialogFragment() {
 
     /**Values**/
     private var mBinding: BottomsheetAddTaskBinding? = null
+    private var vibrator: Vibrator? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         setStyle(DialogFragment.STYLE_NORMAL, R.style.AddTaskBottomSheet)
@@ -40,25 +50,43 @@ class AddTaskBottomSheet : BottomSheetDialogFragment() {
             DataBindingUtil.inflate(inflater, R.layout.bottomsheet_add_task, container, false)
         mBinding?.viewModel = AddTaskViewModel(activity!!)
 
+        vibrator = activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+
         return mBinding?.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mBinding?.viewModel?.setOnTaskAddedListener(object: AddTaskViewModel.OnTaskAddedListener{
+        mBinding?.viewModel?.setOnTaskAddedListener(object : AddTaskViewModel.OnTaskAddedListener {
             override fun onTaskAdded() {
                 dismiss()
                 showSnackBar()
             }
         })
 
-        mBinding?.viewModel?.textError?.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback(){
+        mBinding?.viewModel?.textError?.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
             override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
                 editTextLayout_title.error = (sender as ObservableField<String>).get()
             }
         })
+
+        //Set filter for repetition editText
+        editText_repetition.filters = arrayOf(TextInputFilter(1, 365))
+
+        toggleGroup.setOnSelectListener {
+            if (it.isSelected) {
+                expandable_layout.expand()
+                vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_TICK))
+                showSoftKeyboard(view.findViewById(R.id.editText_repetition))
+            }
+            else {
+                expandable_layout.collapse()
+                hideSoftKeyboard(view.findViewById(R.id.editText_repetition))
+            }
+        }
     }
 
     private fun showSnackBar() {
@@ -70,4 +98,16 @@ class AddTaskBottomSheet : BottomSheetDialogFragment() {
             .show()
     }
 
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm: InputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
+
+    private fun hideSoftKeyboard(view: View) {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
+
---
+++ app/src/main/java/com/sloupycom/shaper/view/Repetition.kt
@@ -0,0 +1,12 @@
+package com.sloupycom.shaper.view
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class Repetition: DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
